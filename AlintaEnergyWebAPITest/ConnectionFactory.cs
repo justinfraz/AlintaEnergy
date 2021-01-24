16e4256e6359f82e479af4035a3372ba2e7c2d93
Justin Frazier <s3625698@student.rmit.edu.au>
2021-01-25 07:12:49 +1100

    AlintaEnergyWebAPI
    Alinta Energy Web API

---
+++ AlintaEnergyWebAPITest/ConnectionFactory.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.IO;
+//using Microsoft.Data.Sqlite;
+using Microsoft.EntityFrameworkCore;
+using AlintaEnergyWebAPI.Models;
+
+namespace AlintaEnergyWebAPITest
+{
+    public class ConnectionFactory : IDisposable
+    {
+        #region IDisposable Support
+        private bool disposedValue = false; // To detect redundant calls
+
+        public CustomerContext CreateContextForInMemory()
+        {
+            var option = new DbContextOptionsBuilder<CustomerContext>().UseInMemoryDatabase(databaseName: "Test_Database").Options;
+
+            var context = new CustomerContext(option);
+            if (context != null)
+            {
+                context.Database.EnsureDeleted();
+                context.Database.EnsureCreated();
+            }
+
+            return context;
+        }
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!disposedValue)
+            {
+                if (disposing)
+                {
+                }
+
+                disposedValue = true;
+            }
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+        }
+        #endregion
+    }
+}
---
+++ AlintaEnergyWebAPITest/InMemoryDataProviderTest.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Linq;
+using AlintaEnergyWebAPI.Controllers;
+using AlintaEnergyWebAPI.Models;
+using Xunit;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace AlintaEnergyWebAPITest
+{
+    public class InMemoryDataProviderTest
+    {
+        [Fact]
+        public void Task_Add_Without_Relation()
+        {
+            //Arrange  
+            var factory = new ConnectionFactory();
+
+            //Get the instance of BlogDBContext
+            var context = factory.CreateContextForInMemory();
+
+            //Act
+            var one = new Customer { FirstName = "John", LastName = "Beasley", DOB = DateTime.Now };
+            // var two = new Customer { FirstName = "Mark", LastName = "Adams", DOB = DateTime.Now };
+            context.Customers.Add(one);
+            context.SaveChanges();
+
+            //Assert  
+            //Get the customer count
+            var customerCount = context.Customers.Count();
+            if (customerCount != 0)
+            {
+                Assert.Equal(1, customerCount);
+            }
+
+            //Get single customer detail
+            var customerPost = context.Customers.FirstOrDefault();
+            if (customerPost != null)
+            {
+                Assert.Equal("John", customerPost.FirstName);
+            }
+        }
+
+        #region Get Customer By Id
+        //[Fact]
+        //public async void Task_GetPostById_Return_OkResult()
+        //{
+        //    //Arrange
+        //    var factory = new ConnectionFactory();
+        //    var context = factory.CreateContextForInMemory();
+
+        //    //Act
+        //    var controller = new CustomersController(context);
+        //    var Id = 1;
+        //    var data = await controller.GetCustomer(Id);
+
+        //    //Assert
+        //    Assert.IsType<OkObjectResult>(data);
+        //}
+
+        //[Fact]
+        //public async void Task_GetPostById_Return_NotFoundResult()
+        //{
+        //    //Arrange
+        //    var factory = new ConnectionFactory();
+        //    var context = factory.CreateContextForInMemory();
+
+        //    //Act
+        //    var controller = new CustomersController(context);
+        //    var Id = 3;
+        //    var data = await controller.GetCustomer(Id);
+
+        //    //Assert
+        //    Assert.IsType<NotFoundResult>(data);
+        //}
+
+        //[Fact]
+        //public async void Task_GetPostById_Return_BadRequestResult()
+        //{
+        //    //Arrange
+        //    var factory = new ConnectionFactory();
+        //    var context = factory.CreateContextForInMemory();
+
+        //    //Act
+        //    var controller = new CustomersController(context);
+        //    int? postId = null;
+
+        //    var data = await controller.GetCustomer(postId);
+
+        //    //Assert
+        //    Assert.IsType<BadRequestResult>(data);
+        //}
+
+        //[Fact]
+        //public async void Task_GetPostById_MatchResult()
+        //{
+        //    //Arrange
+        //    var controller = new PostController(repository);
+        //    int? postId = 1;
+
+        //    //Act
+        //    var data = await controller.GetPost(postId);
+
+        //    //Assert
+        //    Assert.IsType<OkObjectResult>(data);
+
+        //    var okResult = data.Should().BeOfType<OkObjectResult>().Subject;
+        //    var post = okResult.Value.Should().BeAssignableTo<PostViewModel>().Subject;
+
+        //    Assert.Equal("Test Title 1", post.Title);
+        //    Assert.Equal("Test Description 1", post.Description);
+        //}
+        #endregion
+    }
+}
