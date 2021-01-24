e4c680f28464282f1796188977cfbccfe4b6a061
Justin Frazier <s3625698@student.rmit.edu.au>
2021-01-25 07:22:53 +1100

    Additions

---
+++ Models/Customer.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AlintaEnergyWebAPI.Models
+{
+    public class Customer
+    {
+        public long Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public DateTime DOB { get; set; }
+    }
+}
